d3b615f4a7c3ff7385e1bf5ce6f22fbb073c776d
Chesed Kim <starmireu@gmail.com>
2023-04-25 15:43:52 +0900

    added note for test4

--- c/test4/main.c
+++ c/test4/main.c
@@ -4,5 +4,6 @@ int main()
 {
     int variable1, variable2 = 25;
     // variable1 will have random unpredictable or 0 value unless I assign it like variable2.
+    // this action does not touch hash nor node memory.
     return 0;
 }
