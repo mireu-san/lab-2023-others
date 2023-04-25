9e17f6b5cef63e28bcfc245e7fd0167470379b18
Chesed Kim <starmireu@gmail.com>
2023-04-25 15:41:31 +0900

    updated test 4 basic - aware the hash memory

---
+++ c/test3/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int firstNumber = 33;
+    printf("firstNumber = %d", firstNumber);
+
+    int secondNumber = firstNumber;
+    printf("\n secondNumber = %d", secondNumber);
+
+    return 0;
+}
---
+++ c/test4/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int variable1, variable2 = 25;
+    // variable1 will have random unpredictable or 0 value unless I assign it like variable2.
+    return 0;
+}
