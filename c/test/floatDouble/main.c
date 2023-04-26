77d5962880e649c7a93d2cdaee5dbb514b20a1c0
Chesed Kim <starmireu@gmail.com>
2023-04-26 17:18:30 +0900

    float and double

---
+++ c/test/floatDouble/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // double number = 12.45;
+    // float number1 = 10.9f;
+
+    // printf("%.2lf", number);
+    // printf("\n%.1f", number1);
+
+    // 5.5 exponential 6
+    double number = 5.5e6;
+    printf("%lf", number);
+
+    return 0;
+}
