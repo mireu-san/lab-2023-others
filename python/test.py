9e32ae380e8f87655894e645c489931bbc79d82f
Chesed Kim <starmireu@gmail.com>
2023-04-28 17:37:27 +0900

    reprogressing - test.py

---
+++ python/test.py
@@ -0,0 +1,18 @@
+import random
+
+
+def get_choices():
+    player_choice = input("Enter a choice (rock, paper, scissors: ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+
+def check_win(player, computer):
+    print(f"You chose {player}, computer chose {computer}")
+    if player == computer:
+        return "It's a tie!"
+
+
+check_win("rock", "paper")
