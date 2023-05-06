cfa6b6fd683c0a58884074bae9f8b42897810c5a
JONGWAN KIM <starmireu@gmail.com>
2023-05-06 09:00:49 +0900

    filter - exclusion of specific value in list

--- python/test_code_filter.py
+++ python/test_code_filter.py
@@ -1,3 +1,9 @@
 raw_list = ["Alice", "Rabbit", "Card", "Queen", "us", "kr"]
 filtered = list(filter(lambda x: len(x) >= 3, raw_list))
 print(filtered)
+
+filtered_removal = [x for x in raw_list if x != "Card"]
+print(filtered_removal)
+
+multiple_removal = [x for x in raw_list if x not in ["us", "kr"]]
+print(multiple_removal)
