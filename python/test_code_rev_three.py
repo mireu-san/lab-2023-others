4ab457cb2b01c7b46d7d5ef5d5c67c8e33ecda67
JONGWAN KIM <starmireu@gmail.com>
2023-05-08 16:36:20 +0900

    minor update - added link

--- python/test_code_rev_three.py
+++ python/test_code_rev_three.py
@@ -1,3 +1,6 @@
 math_score = {"Alice": 80, "Rabbit": 90, "Queen": 100}
 top_math_score = max(math_score, key=math_score.get)
 print("highest grade earner:", top_math_score)
+
+# https://www.w3schools.com/python/ref_dictionary_get.asp
+# https://www.programiz.com/python-programming/methods/built-in/max
