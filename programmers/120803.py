d2e1cd06e041a6ade366859d4b59fb5a05f53a4a
JONGWAN KIM <starmireu@gmail.com>
2023-05-09 20:01:36 +0900

    first one

---
+++ programmers/120803.py
@@ -0,0 +1,10 @@
+# def solution(num1, num2):
+#     answer = 0
+#     return answer
+
+
+def solution(num1, num2):
+    return num1 - num2
+
+
+# comment : so literally, as long as I meet the requirement, it works regardless initial template itself.
