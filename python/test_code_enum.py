d757d22593e55fb4ac848b601750f0fa4038036f
JONGWAN KIM <starmireu@gmail.com>
2023-05-09 17:15:55 +0900

    use list and enumerate, referring loop variable, child

--- python/test_code_enum.py
+++ python/test_code_enum.py
@@ -2,6 +2,10 @@ children = ["Rei", "Shinji", "Asuka", "Touji", "Kaoru"]
 for i, child in enumerate(children):
     print("the number {} is {}".format(i + 1, child))
 
-# Using list comprehension:
-result = [(i, child) for i, child in enumerate(children)]
+# # Using list comprehension:
+# result = [(i, child) for i, child in enumerate(children)]
+# print(result)
+
+# list comprehension (renewed)
+result = list(enumerate(children, 1))
 print(result)
