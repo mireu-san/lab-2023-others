befe14cc369b35b07b9f7621427f1bde09e4dc1a
JONGWAN KIM <starmireu@gmail.com>
2023-05-09 17:14:25 +0900

    testing enumerate with list comprehension

--- python/test_code_enum.py
+++ python/test_code_enum.py
@@ -1,3 +1,7 @@
 children = ["Rei", "Shinji", "Asuka", "Touji", "Kaoru"]
-for i, val in enumerate(children):
-    print("the number {} is {}".format(i + 1, val))
+for i, child in enumerate(children):
+    print("the number {} is {}".format(i + 1, child))
+
+# Using list comprehension:
+result = [(i, child) for i, child in enumerate(children)]
+print(result)
