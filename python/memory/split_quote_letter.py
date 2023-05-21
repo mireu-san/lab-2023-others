044685530a4f60c5e6af4b53a5d1d479083d6e4b
mireu-san <starmireu@gmail.com>
2023-05-21 11:36:00 +0900

    reviewed

--- python/memory/split_quote_letter.py
+++ python/memory/split_quote_letter.py
@@ -3,11 +3,11 @@ quote = "There is a man who aims to change the world."
 # split the string on whitespace here.
 words = quote.split()
 
-# get first letter of each word here.
-first_letters = [word[0] for word in words]
+# get beginning letter of each word here.
+extractor = [word[0] for word in words]
 
-# join the first letters together.
-joined_first_letters = "".join(first_letters)
+# join the beginning letters together.
+joined_extractor = "".join(extractor)
 
-# print the joined first letters.
-print(joined_first_letters)
+# print the joined beginning letters.
+print(joined_extractor)
--- python/memory/split_quote_letter_two.py
+++ python/memory/split_quote_letter_two.py
@@ -1,29 +1,29 @@
-sentence = "This is how to pick out only the first letter"
+sentence = "This is how to pick out only the beginning letter"
 
 # Create a variable to store the current word.
 current_word = ""
 
-# Create a variable to store the first letter of the current word.
-first_letter = ""
+# Create a variable to store the beginning letter of the current word.
+extractor = ""
 
 # Loop over the characters in the sentence.
 for character in sentence:
-    # If the current character is a space, then we have reached the end of the current word.
+    # If the current character is a space, then it can reach to the end of the current word.
     if character == " ":
-        # Print the first letter of the current word.
-        print(first_letter)
+        # Print the beginning letter of the current word.
+        print(extractor)
 
-        # Reset the current word and first letter variables.
+        # Reset the current word and beginning letter variables.
         current_word = ""
-        first_letter = ""
+        extractor = ""
 
     # Otherwise, this is still in the current word.
     else:
         # Add the current character to the current word.
         current_word += character
 
-        # Set the first letter of the current word to the current character.
-        first_letter = character
+        # Set the beginning letter of the current word to the current character.
+        extractor = character
 
-# Print the first letter of the last word.
-print(first_letter)
+# Print the beginning letter of the last word.
+print(extractor)
