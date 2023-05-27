435cedad89ecf9463186040a69c313e811396c65
mireu-san <starmireu@gmail.com>
2023-05-28 08:18:18 +0900

    181939 - add old one

--- programmers/181939.py
+++ programmers/181939.py
@@ -1,2 +1,12 @@
 def solution(a, b):
     return int(max(f"{a}{b}", f"{b}{a}"))
+
+
+# def solution(a, b):
+#     case1 = int(str(a) + str(b))
+#     case2 = int(str(b) + str(a))
+
+#     if case1 >= case2:
+#         return case1
+#     else:
+#         return case2
