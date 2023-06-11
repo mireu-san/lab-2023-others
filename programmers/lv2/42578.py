affccd42b76e4418df5eb91cde539589a6fba759
mireu-san <starmireu@gmail.com>
2023-06-11 09:46:14 +0900

    hashmap 42578

---
+++ programmers/lv2/42578.py
@@ -0,0 +1,17 @@
+def solution(clothes):
+    hash_map = {}
+    for cloth, type in clothes:
+        hash_map[type] = hash_map.get(type, 0) + 1
+
+    answer = 1
+    for type in hash_map:
+        answer *= hash_map[type] + 1
+
+    return answer - 1
+
+
+print(
+    solution(
+        [["crowmark", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
+    )
+)
