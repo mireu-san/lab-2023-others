81045b7a7ac7dfcbeb476f35faba9a2132e5e91c
mireu-san <starmireu@gmail.com>
2023-06-21 16:43:32 +0900

    fixed db issues along model and updated sqllite DB

--- django/blog/models.py
+++ django/blog/models.py
@@ -6,11 +6,11 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     writer = models.CharField(max_length=10)
-    create_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Comment(models.Model):
     content = models.TextField()
     writer = models.CharField(max_length=10)
-    create_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
