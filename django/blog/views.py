4a50d835aa34526fce2736ac8d15e944cfe25140
mireu-san <starmireu@gmail.com>
2023-06-22 11:26:08 +0900

    update, remove

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
 from .forms import PostForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 # Create your views here.
 # def index(request):
@@ -41,7 +47,7 @@ def write(request):
             return redirect("blog:list")
     # & beware of indentation
     form = PostForm()
-    return render(request, "blog/write.html", {"form": form})
+    return render(request, "blog/post_form.html", {"form": form})
 
 
 # Django 자체의 클래스 뷰 기능도 강력, 편리
@@ -64,3 +70,30 @@ class Detail(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
     context_object_name = "post"  # name of variable = "post"
+
+
+class Update(UpdateView):
+    model = Post
+    template_name = "blog/post_edit.html"
+    fields = ["title", "content"]
+    # success_url = reverse_lazy("blog:list")
+
+    # initial 기능 사용 -> form 에 값을 미리 넣어주기 위해서 # reset 아님. initial.
+    def get_initial(self):
+        # super = approaching parent class to inherit it
+        initial = super().get_initial()  # updateView(generic view)에서 제공하는 initial 딕셔너리.
+        post = self.get_object()  # pk 기반으로.
+        initial["title"] = post.title
+        initial["content"] = post.content
+        return initial
+
+    def get_success_url(self):
+        post = self.get_object()  # pk 기반으로 현재 객체 가져오기 # self 는 class 로 만든, 현재 찍어준 객체
+        # return reverse_lazy("blog:detail", kwargs={"pk: post.pk"})
+        return reverse("blog:detail", kwargs={"pk": post.pk})
+
+
+class Delete(DeleteView):  # 지우는 화면 따로 구성할 필요는 없음.
+    model = Post  # Post 에 있는 값을 지울예정
+    success_url = reverse_lazy("blog:list")
+    # 어디서 실행 시킬까? 상세페이지 내부에서.
