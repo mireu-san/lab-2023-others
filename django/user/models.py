5c43fc03e20ceb8b00fca3589d735090a22317ca
mireu-san <starmireu@gmail.com>
2023-06-26 13:36:58 +0900

    update - User, urls, models

--- django/user/models.py
+++ django/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=255)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    password = models.CharField(max_length=50)
+    registered_date = models.DateTimeField(auto_now_add=True)  # 현재시간
--- django/user/views.py
+++ django/user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# User 관련된 기능
+# 회원가입
+# 로그인
+# 탈퇴
