579c235b314e83decfd7b2945c5ac4f2876fceb1
mireu-san <starmireu@gmail.com>
2023-07-02 16:15:42 +0900

    test out - svelte

---
+++ svelte/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  let name = "mireu-san";
+</script>
+
+<h1>Test out - svelte component</h1>
+<p>This is test</p>
+
+<div>
+  <a href="https://github.com/mireu-san">my github</a>
+</div>
+
+<style>
+  h1 {
+    color: rebeccapurple;
+  }
+  p {
+    font-size: 44px;
+  }
+</style>
