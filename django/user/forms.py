baf54dd18c5735bb727c7fa1906fb56c7c5bd261
mireu-san <starmireu@gmail.com>
2023-07-03 14:14:51 +0900

    user forms - get_user_model = User instead

--- django/user/forms.py
+++ django/user/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-from .models import User
+
+# from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class RegisterForm(forms.ModelForm):
