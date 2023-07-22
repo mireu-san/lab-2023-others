6905b419229a08732e2b017f1d89c2b9d63e5243
mireu-san <starmireu@gmail.com>
2023-07-22 13:54:41 +0900

    sorting problem

---
+++ leetcode/sorting/217_duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hset = set()
+        for idx in nums:
+            if idx in hset:
+                return True
+            else:
+                hset.add(idx)
+
+# 217
