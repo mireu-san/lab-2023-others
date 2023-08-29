94b318ce357561b895b0a7aca748c0dc935f1e42
Chesed Kim <starmireu@gmail.com>
2023-08-29 22:42:59 +0900

    duplicate problem

---
+++ leetcode/top-easy/array/contains_duplicate.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # Initialize an empty set to keep track of seen numbers
+        seen = set()
+        
+        # Loop through each number in the array
+        for num in nums:
+            # If the number is already in the set, it's a duplicate
+            if num in seen:
+                return True
+            # Otherwise, add the number to the set
+            seen.add(num)
+        
+        # If we reach this point, there are no duplicates
+        return False
+    
+
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
