bbfaeacf0ec126b1237b609a83777fdca3172a93
Chesed Kim <starmireu@gmail.com>
2023-11-21 22:03:16 +0900

    practice : algorithm

---
+++ programmers/solved/lv0_120908.py
@@ -0,0 +1,14 @@
+def solution(str1, str2):
+    if str1 == str2:
+        return 1
+
+    l = len(str2)
+    for i in range(len(str1) - 1):
+        if str1[i : i + l].lower() == str2.lower():
+            return 1
+    return 2
+
+
+# 다른이의 해답
+def solution(str1, str2):
+    return 1 if str2 in str1 else 2
