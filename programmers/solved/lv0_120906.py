0d5cd60c40e6dc52c14d927733002af70b9b2723
Chesed Kim <starmireu@gmail.com>
2023-11-24 18:05:08 +0900

    algorithm - 자릿수 더하기

---
+++ programmers/solved/lv0_120906.py
@@ -0,0 +1,11 @@
+def solution(n):
+    n = str(n)
+    total = 0
+
+    for i in n:
+        total += int(i)
+
+    return total
+
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120906
