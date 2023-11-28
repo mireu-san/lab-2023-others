86589fe4e83c95a2bfb3ebad22aa580593a3546a
Chesed Kim <starmireu@gmail.com>
2023-11-28 23:01:55 +0900

    algo : 배열 두배 만들기

---
+++ programmers/solved/lv0_120809.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = [i * 2 for i in numbers]
+
+    return answer
+
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120809
+# https://stackoverflow.com/questions/59997030/array-multiplication-multiplies-occurence-of-values-instead-of-values-itself
