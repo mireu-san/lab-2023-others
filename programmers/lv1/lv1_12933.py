fa9b78e03b1942caa3ba0f45ac6d1a8b40be6c3d
Chesed Kim <starmireu@gmail.com>
2023-12-23 23:20:38 +0900

    정수 내림차순 배치

--- programmers/lv1/lv1_12933.py
+++ programmers/lv1/lv1_12933.py
@@ -7,3 +7,15 @@ def solution(n):
 
 
 # 오히려 이렇게 리스트를 받아서, 정렬 후 int 로 변환해서 join 하는 게 확실함.
+
+
+#     reversed_num = str((n)[::-1])
+#     if n < 0:
+#         reversed_num = "-" + reversed_num[:1]
+#     return int(reversed_num)
+
+
+# def solution(n):
+#     for i in range(n):
+#         i.int(reverse(n))
+#     return n
