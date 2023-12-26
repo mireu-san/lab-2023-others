20009a78f1b0a1a1d8e6a9b55847c5bf7bc4aa2e
Chesed Kim <starmireu@gmail.com>
2023-12-26 22:04:07 +0900

    son in law - mr kim

---
+++ programmers/lv1/lv1_sil_kim.py
@@ -0,0 +1,17 @@
+def solution(seoul):
+    position = seoul.index("Kim")
+    return "김서방은 {}에 있다".format(position)
+
+
+# 오답
+# def solution(seoul):
+#     word = ""
+
+#     for i in word(seoul):
+#         if word in seoul:
+#             return True
+#         else
+#             return False
+#     print("김서방은 {}에 있다")
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/12919
